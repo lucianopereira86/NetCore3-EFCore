18964374bbec3601913ed81b3f510ad74d12d95e
lucianopereira <lucianosousapereira1986@gmail.com>
2019-10-24 23:06:08 -0300

    README

---
+++ Infra/Interface/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace NetCore3WebAPI.Infra.Interface
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IEnumerable<T> List(Expression<Func<T, bool>> expression);
+        bool Any(Expression<Func<T, bool>> expression);
+        T Save(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Infra/Repository/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore3WebAPI.Infra.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NetCore3WebAPI.Infra.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private DbSet<T> entity_;
+        protected DBContext context;
+
+        public BaseRepository(DBContext context)
+        {
+            this.context = context;
+            entity_ = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> List(Expression<Func<T, bool>> expression)
+        {
+            return entity_.Where(expression).ToList();
+        }
+
+        public bool Any(Expression<Func<T, bool>> expression)
+        {
+            return entity_.Any(expression);
+        }
+
+        public T Save(T entity)
+        {
+            entity_.Add(entity);
+            context.SaveChanges();
+
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            entity_.Update(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            entity_.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
